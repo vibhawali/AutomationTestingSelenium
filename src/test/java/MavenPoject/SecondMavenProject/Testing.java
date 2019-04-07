cc25d8d36d8356e12800706e1db4daa4dc652cd3
vibhawali <vbhwali@gmail.com>
2019-04-07 17:57:25 +0530

    asdas

---
+++ src/test/java/MavenPoject/SecondMavenProject/Testing.java
@@ -0,0 +1,13 @@
+package MavenPoject.SecondMavenProject;
+
+import org.testng.annotations.Test;
+
+public class Testing 
+{
+
+	@Test
+	public void test1()
+	{
+		System.out.println("testing");
+	}
+}
